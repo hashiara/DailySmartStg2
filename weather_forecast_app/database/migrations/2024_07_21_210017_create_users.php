07c6fc695ebf089c282190dc0514f015c5bb0b79
moheros <qxin829@gmail.com>
2024-07-21 21:36:17 +0900

    migration修正6

--- weather_forecast_app/database/migrations/2024_07_21_210017_create_users.php
+++ weather_forecast_app/database/migrations/2024_07_21_210017_create_users.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('mail', 255)->unique();   // メールアドレス
             $table->string('password', 12);          // パスワード
             $table->string('user_name', 50);         // ユーザー名
-            $table->integer('birth', 8);             // 生年月日
+            $table->date('birth');                   // 生年月日
             $table->string('otk', 12)->unique();     // ワンタイム認証キー
             $table->timestamps();                    // 作成日時 更新日時
             $table->softDeletes();                   // 削除日時
